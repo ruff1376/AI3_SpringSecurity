4980a06cee6737191b6859606caf160cf1172b31
ruff1376 <ruff1376@gmail.com>
2025-06-25 17:42:33 +0900

    Spring Security

---
+++ security/src/main/java/com/aloha/security/domain/Users.java
@@ -0,0 +1,20 @@
+package com.aloha.security.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Users {
+    private Long no;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private Date createdAt;
+    private Date updatedAt;
+    private int enabled;
+
+    private List<UserAuth> authList;
+}
---
+++ security/src/main/java/com/aloha/security/service/UserService.java
@@ -0,0 +1,27 @@
+package com.aloha.security.service;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.aloha.security.domain.UserAuth;
+import com.aloha.security.domain.Users;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public interface UserService {
+    
+    // 회원 가입
+    public int join(Users user) throws Exception;
+    
+    // 회원 권한 등록
+    public int insertAuth(UserAuth userAuth) throws Exception;
+
+    // 🔐 로그인
+    public boolean login(Users user, HttpServletRequest request);
+
+    // 회원 조회
+    public Users select(String username) throws Exception;
+
+    // 👮‍♂️ 관리자 확인
+    public boolean isAdmin() throws Exception;
+    
+}
